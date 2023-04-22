2eb07abc3134ea4bc374e7247ba23b8ca82330d9
AsiaWi <joannawitek@icloud.com>
2023-04-22 00:54:09 +0000

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,28 @@ document.addEventListener('DOMContentLoaded', function(){
                 alert ('You clicked Submit!');
             } else {
                 let gameType= this.getAttribute('data-type');
-                alert(`you clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame('addition');
 })
+/** 
+ * The main game 'loop' called when the script is first loaded
+ * and after the users answers have been processed
+ */
+function runGame(gameType) {
+    let num1= Math.floor(Math.random() * 25)+1;
+    let num2= Math.floor(Math.random() * 25)+1;
 
-function runGame() {
 
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else{
+        alert('unknown game type: ${gameType');
+        throw 'unknown game type: ${gameType}. Aborting!';
+    }
 }
 
 function checkAnswer() {
@@ -36,8 +50,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+       document.getElementById('operand1').textContent = operand1;
+       document.getElementById('operand2').textContent = operand2;
+       document.getElementById('operator').textContent = '+';
 
 }
 
